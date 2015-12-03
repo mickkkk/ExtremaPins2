94a937d8ee19549b909eee201035817993f8b2d7
Mick Vranken <mickyvranken@hotmail.com>
2015-12-03 10:54:39 +0100

    Initial Commit

---
+++ ExtremaPins2Tests/ExtremaPins2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ExtremaPins2Tests.swift
+//  ExtremaPins2Tests
+//
+//  Created by Fhict on 03/12/15.
+//  Copyright © 2015 Mick Vranken. All rights reserved.
+//
+
+import XCTest
+@testable import ExtremaPins2
+
+class ExtremaPins2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
